b74e918917a401c1f802dc1b9e6bc52f1f829aee
Michael Gehrmann <michael.gehrmann@gmail.com>
2020-04-18 23:40:02 +0200

    post template

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,21 @@
-module.exports = {
-    dir: {
-        input: "./_source"
-        
-    },
-    templateFormats: [
-        "md",
-        "css",
-        "njk", 
-        "yml", 
-        "html"
-    ],
-    passthroughFileCopy: true
+
+
+module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addLayoutAlias('blog', 'post.njk');
+
+    return {
+        dir: {
+            input: "./_source"   
+        },
+        templateFormats: [
+            "md",
+            "css",
+            "njk", 
+            "yml", 
+            "html"
+        ],
+        passthroughFileCopy: true
+    }
+
 };
